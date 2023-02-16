060c408998d2628fd9f44f265f063c48c8aab34d
sezare <sezare229@gmail.com>
2023-02-16 11:53:20 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "with proper grammar, but the outcome is 
  * a piece of art," followed by a newline.
  * Return: Always 0.
  *
  */
+
 
 int main(void)
 
