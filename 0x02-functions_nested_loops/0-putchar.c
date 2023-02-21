c41eced7b264ce5fa854ff76b5faa224b74889ca
sezare <sezare229@gmail.com>
2023-02-21 09:01:33 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,14 @@
  */
 
 int main(void)
-{
-	_putchar('_');
+{	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');  
 	_putchar('h');
-	_putchar('q');
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
-
