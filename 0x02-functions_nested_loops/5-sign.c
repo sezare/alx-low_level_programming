94941282a498e81972a945fded723e1244c993c9
sezare <sezare229@gmail.com>
2023-02-22 11:34:39 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0 )
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+	_putchar('\n');
+}
+	
+
