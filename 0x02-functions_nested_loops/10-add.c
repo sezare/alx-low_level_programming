a995f8a687c76f575386f8f4655303f45f8093ed
sezare <sezare229@gmail.com>
2023-02-22 14:12:06 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * add - Add to values
  * @n1: The first number to be counted 
  * @n2: The second number to be computed
- * Return: Integer value
+ * Return: Integer value.
  */
 int add(int n1, int n2)
 {
