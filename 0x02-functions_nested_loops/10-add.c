49789277ce59bbefe5a6ac6a03ff31c23e7a0822
sezare <sezare229@gmail.com>
2023-02-22 14:08:56 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Add to values
+ * @n1: The first number to be counted 
+ * @n2: The second number to be computed
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
