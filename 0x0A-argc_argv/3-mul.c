91783ad01fb88a8ae54248172ec75bea2eb8fcf7
sezare <sezare229@gmail.com>
2023-03-11 13:09:05 -0800

    Agrc vs Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,22 +7,15 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
@@ -37,24 +30,28 @@ int _atoi(char *s)
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-			digit = -digit;
+				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (
+s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
-		return (n);
+
+	return (n);
 }
+
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
@@ -62,13 +59,16 @@ int main(int argc, char *argv[])
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
-
-	{	printf("Error\n");
+	{
+		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
