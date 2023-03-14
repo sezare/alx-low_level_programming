a16d774cadcdefef1474e3ed6cec52829f2f167c
sezare <sezare229@gmail.com>
2023-03-13 22:58:23 -0700

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,27 +11,25 @@ char *_strdup(char *str)
 	char *dup;
 	int i, len = 0;
 	/** Check for a NULL input string*/
-	if (str == NULL){
-		return NULL;
-	}
+	if (str == NULL)
+		return (NULL);
 
 	/**Compute the length of the input string*/
 	while (str[len] != '\0')
-        len++;
+		len++;
 
 	/** Allocate memory for the duplicate string*/
 	dup = malloc((len + 1) * sizeof(char));
-	if (dup == NULL){
-		return NULL;
-	}
-	
+	if (dup == NULL)
+
+		return (NULL);
+
 	/**Copy the input string to the duplicate string*/
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
 	dup[len] = '\0'; /**add null terminator to end of duplicate string*/
-		{
+
 
 	/**Return a pointer to the duplicate string*/
-	return dup;
-		}
+	return (dup);
 }
