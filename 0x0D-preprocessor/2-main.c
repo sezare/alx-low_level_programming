510f972fd99047de22f353d39de22b15ea580338
sezare <sezare229@gmail.com>
2023-03-20 06:55:35 -0700

    The typedef#

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - function to print the name of the file it was compiled from
- *   *
- *    * Return: 0 for success
- *     */
+* main - function to print the name of the file it was compiled from
+*
+* Return: 0 for success
+*/
 int main(void)
 {
 		printf("%s\n", __FILE__);
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef abs_h
-#define abs_h
-#define ABS(x) (x > 0 ? x : x * -1)
-#endif
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+
+#endif /* FUNCTION_LIKE_MACRO */
