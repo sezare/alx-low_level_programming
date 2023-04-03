52c9061e24bcaaca3541ef3b0ac6e4648088a784
sezare <sezarem229@gmail.com>
2023-04-03 14:45:05 -0700

    Single link

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,12 @@
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
-{
- 	size_t num = 0;
-
-	 while (h != NULL)
-	 {
-	 printf("%d\n", h->n);
-	 num++;
-	 h = h->next;
-	 }
-
-	 return (num);
+{	size_t num = 0;
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	num++;
+	h = h->next;
+	}
+	return (num);
 }
