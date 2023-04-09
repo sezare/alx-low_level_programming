8a51c95e6687919561de1da48c90d531c70f5cf1
sezare <sezarem229@gmail.com>
2023-04-09 01:37:15 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read and print text file to stdout machine
+ * @filename: ptr to the file to read and print on the screen
+ * @letters: number of bytes to print out
+ * Return: if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t reader, output, fd;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	reader = read(fd, buff, letters);
+
+	if (reader == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	output = write(STDOUT_FILENO, buff, reader);
+
+	if (output == -1 || output != reader)
+	{
+		free(buff);
+		return (0);
+	}
+
+	close(fd);
+	free(buff);
+	return (output);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,12 @@
+General
+
+
+1. Look for the right source of information online
+2. How to create, open, close, read and write files
+3. What are file descriptors
+4. What are the 3 standard file descriptors, what are their purpose and what are their POSIX names
+5. How to use the I/O system calls open, close, read and write
+6. What are and how to use the flags O_RDONLY, O_WRONLY, O_RDWR
+7. What are file permissions, and how to set them when creating a file with the open system call
+8. What is a system call
+9. What is the difference between a function and a system call
